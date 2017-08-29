a82085a280deebd825fd7c2fb8822756fa1644d8
Lonestar21709 <lilceez21@hotmail.com>
2017-08-29 02:52:30 -0500

    QUICKSORT

--- Random/App.java
+++ Random/App.java
@@ -7,15 +7,8 @@ import java.util.Random;
 public class App {
 	
 	public static void main(String args[]){
-		//Not sure how to do this one, specifically making the file, i would need more time to figure out the required methods 
-		//to generate a file.
-		Random generateNum = new Random();
-		for(int i = 1; i <= 10; i++){
-			int randomNum = generateNum.nextInt(10000);
-		}
-		//would store the numbers that are largest into a data structure, probably just an array.  Then I would have to 
-		//create  a nest of loops, first ten numbers would fill the the array, then after that you're just looping through the array
-		//on each number from the file.  Until it goes through all the numbers.  
+		//need to implement quicksort algo. 
+		
 		
 	}
 }
